05633cb7c4bddb7f72ae4fb7440c7ebe6788a4a6
Wojciech Korzybski <wojciech.korzybski@gmail.com>
2023-05-15 13:01:09 +0200

    Collector and trainer. Cart pole demo

--- main.py
+++ main.py
@@ -44,3 +44,131 @@ class Net(nn.Module):
         entropy = distribution.entropy()
         Qvalue = self.critic(observation)
         return logarithm_probabilities, torch.squeeze(Qvalue), entropy
+
+class DataCollector:
+    def __init__(self, net, out_num, environment_name, gamma):
+        self.net = net
+        self.out_num = out_num
+        self.env = gym.make(environment_name, render_mode="rgb_array")
+        self.gamma = gamma
+        self.rewards = []
+        self.action_logarithms = []
+        self.states = []
+        self.render = False
+        self.actions = []
+        self.Qval = 0
+
+    def clear_previous_batch_data(self):
+        self.np_Qvals = []
+        self.rewards = []
+        self.action_logarithms = []
+        self.states = []
+        self.actions = []
+        self.Qval = 0
+
+    def calculate_qvals(self):
+        Qval = 0
+        Qvals = []
+        for reward in reversed(self.rewards):
+            Qval = reward + self.gamma * Qval
+            Qvals.insert(0, Qval)
+        return torch.tensor(Qvals)
+
+    def collect_data_for(self, batch_size):
+        current_state = self.env.reset()[0]
+        print(np.array(current_state[0], dtype=np.float32))
+        for simulation_step in range(batch_size):
+
+            if self.render:
+                img = cv2.cvtColor(self.env.render(), cv2.COLOR_RGB2BGR)
+                cv2.imshow("test", img)
+                cv2.waitKey(50)
+
+            action = self.net.pick_action(current_state, self)
+            observation, reward, done, _, _ = self.env.step(action)
+            self.rewards.append(reward)
+            current_state = observation
+            if done or simulation_step == batch_size - 1:
+                self.Qval = self.calculate_qvals()
+                break
+
+    def stack_data(self):
+        self.states = torch.stack(self.states)
+        self.actions = torch.stack(self.actions)
+        self.action_logarithms = torch.stack(self.action_logarithms)
+
+
+class A2CTrainer:
+    def __init__(self, net, out_num, environment_name, batch_size,
+                 gamma, beta_entropy, learning_rate, clip_size):
+
+        self.net = net
+        self.new_net = copy.deepcopy(net)
+        self.batch_size = batch_size
+        self.beta_entropy = beta_entropy
+        self.learning_rate = learning_rate
+        self.clip_size = clip_size
+        self.optimizer = torch.optim.Adam(
+            self.new_net.parameters(), lr=self.learning_rate)
+        self.data = DataCollector(net, out_num, environment_name, gamma)
+
+    def calculate_actor_loss(self, ratio, advantage):
+        opt1 = ratio * advantage
+        opt2 = torch.clamp(ratio, 1 - self.clip_size,
+                           1 + self.clip_size) * advantage
+        return (-torch.min(opt1, opt2)).mean()
+
+    def calculate_critic_loss(self, advantage):
+        return 0.5 * advantage.pow(2).mean()
+
+    def train(self):
+        self.data.clear_previous_batch_data()
+        self.data.collect_data_for(batch_size=self.batch_size)
+        self.data.stack_data()
+
+        action_logarithms, Qval, entropy = self.new_net.evaluate(
+            self.data.states, self.data.actions)
+
+        ratio = torch.exp(action_logarithms -
+                          self.data.action_logarithms.detach())
+        advantage = self.data.Qval - Qval.detach()
+        actor_loss = self.calculate_actor_loss(ratio, advantage)
+        critic_loss = self.calculate_critic_loss(advantage)
+
+        loss = actor_loss + critic_loss + self.beta_entropy * entropy.mean()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+        self.net.load_state_dict(self.new_net.state_dict())
+        return sum(self.data.rewards), self.net
+
+if __name__ == '__main__':
+    CART_POLE_IN_NUM = 4
+    CART_POLE_OUT_NUM = 2
+    NUM_OF_EPISODES = 1000
+    RENDER_INTERVAL = 50
+    trainer = A2CTrainer(net=Net(CART_POLE_IN_NUM, CART_POLE_OUT_NUM),
+                         out_num=CART_POLE_OUT_NUM,
+                         environment_name='CartPole-v1',
+                         batch_size=500,
+                         gamma=0.99,
+                         beta_entropy=0.001,
+                         learning_rate=0.001,
+                         clip_size=0.2)
+    best_result = 0
+    in_which_episode = 0
+    for episode in range(NUM_OF_EPISODES):
+        if episode % RENDER_INTERVAL == 0:
+            trainer.data.render = True
+        else:
+            trainer.data.render = False
+
+        curr_result, _ = trainer.train()
+
+        if curr_result > best_result:
+            best_result = curr_result
+            in_which_episode = episode
+        print(
+            f'{episode}. {curr_result}\tBest: {best_result} in episode {in_which_episode}')
